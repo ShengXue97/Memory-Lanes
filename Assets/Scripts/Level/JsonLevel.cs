4133e29c311f4502bd6ef64cf8784e4ef353417e
PotatoCombat <39809238+PotatoCombat@users.noreply.github.com>
2021-01-20 16:13:28 +0800

    Add JsonTriggerEvent
    Includes:
    - Ability to create event from Json

--- Assets/Scripts/Level/JsonLevel.cs
+++ Assets/Scripts/Level/JsonLevel.cs
@@ -3,6 +3,27 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [Serializable]
+public struct JsonTriggerEvent
+{
+    // Coordinates of trigger
+    public int x;
+    public int y;
+
+    // actions: 'On', 'Off',
+    public string action;
+    
+    // public string type; // type of trigger - currently the only type is 'Switch'
+
+    public TriggerEvent CreateFromJson(Level level)
+    {
+        TriggerEvent triggerEvent = new TriggerEvent();
+        triggerEvent.trigger = level.GetForegroundObject(x, y).GetComponent<MyTrigger>();
+        Enum.TryParse(action, false, out triggerEvent.action);
+        return triggerEvent;
+    }
+}
+
+[Serializable]
 public struct JsonActivatorEvent
 {
     // Coordinates of activator
